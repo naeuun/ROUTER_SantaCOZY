1ad379471e2d40111c30586a20e03cbcabe618a9
naeun <1210naeun@gmail.com>
2025-11-18 20:45:32 +0900

    ✨ Feat : index,load,result

--- santaCozy/urls.py
+++ santaCozy/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from .views import *
 
+app_name = 'santaCozy'
+
 urlpatterns = [
     path('', index, name='index'),
+    path('loading/', loading, name='loading'),
     path('result/', result, name='result'),
 ]
--- santaCozy/views.py
+++ santaCozy/views.py
@@ -3,5 +3,8 @@ from django.shortcuts import render
 def index(request):
     return render(request, 'santaCozy/index.html')
 
+def loading(request):
+    return render(request, 'santaCozy/loading.html')
+
 def result(request):
     return render(request, 'santaCozy/result.html')

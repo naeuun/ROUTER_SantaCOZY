032dd36aad09316e017821803f781c6622bbb70e
leeyumin626 <leeym7404@naver.com>
2025-12-03 21:56:50 +0900

    requirements 추가

--- config/settings.py
+++ config/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = 'django-insecure-ea$l8)&j-anyjarx4a4z16@crgz*p@4n!u)m*1b2s(#)ew@6m*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
